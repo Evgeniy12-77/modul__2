9f2e4fd6f998cd69c886d070e7e710eb0266aefa
Евгений Толкач <tolkachevgeniy007@gmail.com>
2023-10-15 02:50:10 +0300

    сделали рекламное задание

---
+++ lesson 2_3/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const modalTitle = document.querySelector('.modal__title');
+console.log(modalTitle);
+const modalForm = document.querySelector('.modal__form');
+console.log (modalForm);
+const checbox = document.querySelector('.modal__submit');
+console.log(checbox);
+const overlay = document.querySelector('.overlay');
+console.log(overlay);
+
+overlay.remove('.active');
+
+const createRow = Object => {
+return 
+}
